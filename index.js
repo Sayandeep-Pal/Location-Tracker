c9a2385b58e9d4d9f9a9d9dc719d683120396b4c
Sayandeep-Pal <145487074+Sayandeep-Pal@users.noreply.github.com>
2025-02-26 18:16:09 +0530

    all files are created

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const path = require('path');
+const server = http.createServer(app);
+const soketio = require("socket.io");
+
+const io = soketio(server);
+
+app.set('view engine', 'ejs');
+app.set(express.static(path.join(__dirname, 'public',)));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
